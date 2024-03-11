a152dbf43b2c89b4d9d3e702fe2e9a884c5e02bf
Lucas Silveira <lucaszaib@gmail.com>
2024-03-11 13:43:14 -0300

    header sicoob 6

--- comparador.py
+++ comparador.py
@@ -160,7 +160,7 @@ def main():
             if file_extension in ['.xlsx', '.xls']:
 
                 if uploaded_sicoob is not None and uploaded_file2 is not None:
-                    df1 = load_dataframe(uploaded_sicoob, header=2)  # Define o cabeçalho na linha 3
+                    df1 = load_dataframe(uploaded_sicoob, header=6)  # Define o cabeçalho na linha 3
                     df2 = load_dataframe(uploaded_file2)
 
                     st.subheader("Conteúdo do arquivo 1")
